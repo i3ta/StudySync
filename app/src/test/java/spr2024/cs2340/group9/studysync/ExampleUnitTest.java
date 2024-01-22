3ba0bcc8e63159929f3f440c756cfc207d514070
Aaron Hung <ahx5x16+github@gmail.com>
2024-01-22 17:44:44 -0500

    ### Summary
    Created Android studio project
    
    ### Description
    
    - Used the "Basic Views Activity" to initialize Android project

---
+++ app/src/test/java/spr2024/cs2340/group9/studysync/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package spr2024.cs2340.group9.studysync;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
