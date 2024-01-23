ed562aebf2676f9cb73168500ee2bf3a1366689e
Aaron Hung <ahx5x16+github@gmail.com>
2024-01-22 19:36:24 -0500

    Imported dependencies to app: - Room 2.6.1 (for managing database) - RxJava 3.1.8 (for async database requests)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,3 +45,32 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+dependencies {
+    implementation("io.reactivex.rxjava3:rxjava:3.1.8")
+}
+
+dependencies {
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
+}
