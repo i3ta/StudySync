3abf4792e08a5511b57aa4bab6e7d640157f373c
Aaron Hung <ahx5x16+github@gmail.com>
2024-01-22 23:51:11 -0500

    Added documentation for Exams.java

--- app/src/main/java/spr2024/cs2340/group9/studysync/database/Exams.java
+++ app/src/main/java/spr2024/cs2340/group9/studysync/database/Exams.java
@@ -10,6 +10,10 @@ public class Exams {
     private static ExamDatabase examDB;
     private static ExamDao examDao;
 
+    /**
+     * Create new instance of the Exam database.
+     * @param applicationContext context of the current application; use getApplicationContext() to get
+     */
     public Exams(Context applicationContext) {
         if (examDB == null) {
             examDB = Room.databaseBuilder(applicationContext, ExamDatabase.class, "Exam").build();
@@ -19,26 +23,54 @@ public class Exams {
         }
     }
 
+    /**
+     * Insert exams.
+     * @param exams exams to insert
+     */
     public void insert(Exam... exams) {
         examDao.insert(exams);
     }
 
+    /**
+     * Delete an exam.
+     * @param exam exam to delete
+     */
     public void delete(Exam exam) {
         examDao.delete(exam);
     }
 
+    /**
+     * Delete an exam with the id.
+     * @param id exam id to delete
+     */
     public void delete(int id) {
         examDao.delete(id);
     }
 
+    /**
+     * Get all exams.
+     * @return exams
+     */
     public Exam[] getAll() {
         return examDao.getAll();
     }
 
+    /**
+     * Get all exams between startTime and endTime.
+     * @param startTime start of time frame
+     * @param endTime end of time frame
+     * @return exams
+     */
     public Exam[] getBetween(Date startTime, Date endTime) {
         return examDao.getBetween(startTime.getTime(), endTime.getTime());
     }
 
+    /**
+     * Get all exams with a specific ordering scheme.
+     * @param order ordering scheme
+     * @return exams
+     * @throws IllegalArgumentException if Order.DUE_DATE is passed in
+     */
     public Exam[] getAll(Order order) throws IllegalArgumentException {
         if (order == Order.DUE_DATE) {
             throw new IllegalArgumentException("Exams can not be ordered by due date.");
