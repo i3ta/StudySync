a18356cc79cd4bac018d7a3366f6e1459ccb51ce
Aaron Hung <ahx5x16+github@gmail.com>
2024-01-22 22:34:05 -0500

    Created databases for Course and CourseTime objects

---
+++ app/src/main/java/spr2024/cs2340/group9/studysync/database/CourseDatabase.java
@@ -0,0 +1,9 @@
+package spr2024.cs2340.group9.studysync.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Course.class}, version = 1)
+public abstract class CourseDatabase extends RoomDatabase {
+    public abstract CourseDao courseDao();
+}
---
+++ app/src/main/java/spr2024/cs2340/group9/studysync/database/CourseTimeDatabase.java
@@ -0,0 +1,9 @@
+package spr2024.cs2340.group9.studysync.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {CourseTime.class}, version = 1)
+public abstract class CourseTimeDatabase extends RoomDatabase {
+    public abstract CourseTimeDao courseTimeDao();
+}
