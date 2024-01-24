aea45046c108d1b2b13f37d4dbf5add6df12890b
Aaron Hung <ahx5x16+github@gmail.com>
2024-01-24 08:51:14 -0500

    Added new constructors to CourseTime to make creating objects easier. Added private static id to control which id a new object should be.

--- app/src/main/java/spr2024/cs2340/group9/studysync/database/CourseTime.java
+++ app/src/main/java/spr2024/cs2340/group9/studysync/database/CourseTime.java
@@ -1,6 +1,7 @@
 package spr2024.cs2340.group9.studysync.database;
 
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity
@@ -11,4 +12,27 @@ class CourseTime {
     public int courseId;
     public int startTime;
     public int endTime;
+
+    @Ignore
+    private static int currentId = 0;
+
+    public CourseTime(int id, int courseId, int startTime, int endTime) {
+        this.id = id;
+        this.courseId = courseId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public CourseTime() {
+        id = currentId++;
+    }
+
+    public CourseTime(int courseId, int startTime, int endTime) {
+        this(currentId++, courseId, startTime, endTime);
+    }
+
+    @Ignore
+    static int getNewId() {
+        return currentId++;
+    }
 }
