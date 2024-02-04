62bae7b65211f97238c32e7ec2e540130e72b116
aaronhung <ahx5x16+github@gmail.com>
2024-02-03 21:25:00 -0500

    Attached exams fragment to exam button in bottomNavigationBar

--- app/src/main/java/spr2024/cs2340/group9/studysync/ExamFragment.java
+++ app/src/main/java/spr2024/cs2340/group9/studysync/ExamFragment.java
@@ -9,82 +9,49 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.TimePicker;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
+import spr2024.cs2340.group9.studysync.adapters.ExamAdapter;
+import spr2024.cs2340.group9.studysync.database.Exam;
+import spr2024.cs2340.group9.studysync.database.Exams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 
-import spr2024.cs2340.group9.studysync.adapters.ExamAdapter;
-import spr2024.cs2340.group9.studysync.database.Exam;
-import spr2024.cs2340.group9.studysync.database.Exams;
-
-/**
- * Exam Fragment.
- */
 public class ExamFragment extends Fragment {
+
+    private Button addButton;
+    private ListView examListView;
+    private Exams examsHelper;
     private ExamAdapter examAdapter;
 
-    /**
-     * Creates Exam view
-     * @param inflater The LayoutInflater object that can be used to inflate
-     * any views in the fragment,
-     * @param container If non-null, this is the parent view that the fragment's
-     * UI should be attached to.  The fragment should not add the view itself,
-     * but this can be used to generate the LayoutParams of the view.
-     * @param savedInstanceState If non-null, this fragment is being re-constructed
-     * from a previous saved state as given here.
-     *
-     * @return View created
-     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.exams_fragment, container, false);
 
-        Button addButton = view.findViewById(R.id.add_button);
-        addButton.setOnClickListener(v -> showDateTimeInputDialog());
+        addButton = view.findViewById(R.id.add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimeInputDialog();
+            }
+        });
 
         // Initialization
-        ListView examListView = view.findViewById(R.id.listView);
-        examAdapter = new ExamAdapter(requireContext(), new ArrayList<>());
+        examListView = view.findViewById(R.id.listView);
+        examAdapter = new ExamAdapter(requireContext(), new ArrayList<Exam>());
         examListView.setAdapter(examAdapter);
-        Exams.init(requireContext());
+        examsHelper = new Exams();
+        examsHelper.init(requireContext());
+
+//        examsHelper.clear();
 
         // Load exams from the database
         updateExamListView();
 
-        // Long hold response
-        examListView.setOnItemLongClickListener((parent, view1, position, id) -> {
-            TextView myInvisibleView = view1.findViewById(R.id.hiddenId);
-            String valueString = myInvisibleView.getText().toString();
-            int examId = Integer.parseInt(valueString);
-            Exam selectedExam = Exams.get(examId);
-
-            // Build an alert with code
-            androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(requireContext(), R.style.AlertDialogTheme);
-            builder.setTitle("Delete To Do List");
-            builder.setMessage("Are You Sure?");
-            builder.setPositiveButton("Yes", (dialog, which) -> {
-                Exams.delete(selectedExam.getId());
-                updateExamListView();
-            });
-            builder.setNegativeButton("Cancel", (dialog, which) -> {});
-            androidx.appcompat.app.AlertDialog dialog = builder.create();
-            dialog.show();
-
-            return true;
-        });
-
         return view;
     }
 
-    /**
-     * Initializes dateTimeInputDialog.
-     */
     private void showDateTimeInputDialog() {
         View dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_layout, null);
 
@@ -93,8 +60,7 @@ public class ExamFragment extends Fragment {
         TimePicker timePicker = dialogView.findViewById(R.id.timePicker);
         EditText titleEditText = dialogView.findViewById(R.id.titleEditText);
         EditText locationEditText = dialogView.findViewById(R.id.locationEditText);
-        Button okButton = dialogView.findViewById(R.id.save_button);
-        Button cancelButton = dialogView.findViewById(R.id.cancel_button);
+        Button okButton = dialogView.findViewById(R.id.okButton);
 
         // Set up date picker
         final Calendar calendar = Calendar.getInstance();
@@ -115,62 +81,45 @@ public class ExamFragment extends Fragment {
                 .setView(dialogView)
                 .create();
 
-        okButton.setOnClickListener(v -> {
-            // Get the selected date from DatePicker
-            int year = datePicker.getYear();
-            int month = datePicker.getMonth();
-            int dayOfMonth = datePicker.getDayOfMonth();
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the selected date from DatePicker
+                int year = datePicker.getYear();
+                int month = datePicker.getMonth();
+                int dayOfMonth = datePicker.getDayOfMonth();
 
-            // Get the selected time from TimePicker
-            int hourOfDay = timePicker.getHour();
-            int minute = timePicker.getMinute();
+                // Get the selected time from TimePicker
+                int hourOfDay = timePicker.getHour();
+                int minute = timePicker.getMinute();
 
-            // Get the text from EditText
-            String userInput = titleEditText.getText().toString();
-            String userInput1 = locationEditText.getText().toString();
+                // Get the text from EditText
+                String userInput = titleEditText.getText().toString();
+                String userInput1 = locationEditText.getText().toString();
 
-            // Construct Calendar object from selected date and time
-            Calendar selectedDateTime = Calendar.getInstance();
-            selectedDateTime.set(year, month, dayOfMonth, hourOfDay, minute);
+                // Construct Calendar object from selected date and time
+                Calendar selectedDateTime = Calendar.getInstance();
+                selectedDateTime.set(year, month, dayOfMonth, hourOfDay, minute);
 
-            Exam newExam = new Exam(userInput, userInput1, selectedDateTime.getTimeInMillis(), 0);
-            Exams.insert(newExam);
+                Exam newExam = new Exam(userInput, userInput1, selectedDateTime.getTimeInMillis(), 0);
+                examsHelper.insert(newExam);
 
-            // Update the ListView
-            updateExamListView();
+                // Update the ListView
+                updateExamListView();
 
-            dialog.dismiss();
+                dialog.dismiss();
+            }
         });
-
-        cancelButton.setOnClickListener(v -> dialog.dismiss());
-
         dialog.show();
     }
 
-    /**
-     * Updates exam list view with updated database.
-     */
     private void updateExamListView() {
         // Load exams from the database using your Exams helper class
-        Exam[] exams = Exams.getAll();
+        Exam[] exams = examsHelper.getAll();
 
         // Update the adapter with the loaded exams
         examAdapter.clear();
         examAdapter.addAll(Arrays.asList(exams));
     }
 
-    /**
-     * Hides action bar
-     */
-    @Override
-    public void onResume() {
-        super.onResume();
-        ((AppCompatActivity) requireActivity()).getSupportActionBar().hide();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        ((AppCompatActivity) requireActivity()).getSupportActionBar().show();
-    }
 }
--- app/src/main/java/spr2024/cs2340/group9/studysync/MainActivity.java
+++ app/src/main/java/spr2024/cs2340/group9/studysync/MainActivity.java
@@ -7,7 +7,6 @@ import com.google.android.material.navigation.NavigationBarView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,18 +17,21 @@ import android.view.MenuItem;
 
 import spr2024.cs2340.group9.studysync.databinding.ActivityMainBinding;
 
-/**
- * Main activity.
- */
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private UpcomingFragment upcomingFragment = new UpcomingFragment();
+
+    // Fragments for testing
+    private FirstFragment firstFragment = new FirstFragment();
+    private ExamFragment secondFragment = new ExamFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        spr2024.cs2340.group9.studysync.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setSupportActionBar(binding.toolbar);
         setContentView(binding.getRoot());
 
@@ -40,21 +42,20 @@ public class MainActivity extends AppCompatActivity {
         // Get a reference to the BottomNavigationView
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        // Set the listener for the bottom navigation view
+        // Set the listener
         bottomNavigationView.setOnItemSelectedListener(navListener);
+
+        System.out.println("Set up main activity with listener.");
     }
 
-    /**
-     * Lambda for BottomNavigationView listener.
-     */
     private final NavigationBarView.OnItemSelectedListener navListener = (item) -> {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         int itemId = item.getItemId();
 
         if (itemId == R.id.upcoming) {
             navController.navigate(R.id.upcomingFragment);
-        } else if (itemId == R.id.todo) {
-            navController.navigate(R.id.toDoListFragment);
+        } else if (itemId == R.id.courses) {
+            navController.navigate(R.id.firstFragment);
         } else if (itemId == R.id.exams) {
             navController.navigate(R.id.examFragment);
         } // Add other navigation actions here
